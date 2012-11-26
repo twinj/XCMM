0a353c820748090a8908bd527a83f11340eb4a35
Daniel Kemp <twinjudgewarriro@hotmail.com>
2012-11-26 15:56:49 +1000

    Uncompressed files now thrown with Exception

--- src/org/xcom/mod/tools/exceptions/UpkFileNotDecompressedException.java
+++ src/org/xcom/mod/tools/exceptions/UpkFileNotDecompressedException.java
@@ -1,13 +1,27 @@
 package org.xcom.mod.tools.exceptions;
 
+import java.nio.file.Path;
+import java.util.List;
+
 public class UpkFileNotDecompressedException extends Exception {
 
 	private static final long serialVersionUID = 1L;
+	
+	List<Path> uncompedFiles;
 
-	public UpkFileNotDecompressedException() {
+	public UpkFileNotDecompressedException(List<Path> uncompedFiles) {
+		this.uncompedFiles = uncompedFiles;
 	}
 
-	public UpkFileNotDecompressedException(String msg) {
+	public UpkFileNotDecompressedException(List<Path> uncompedFiles, String msg) {
 		super(msg);
+		this.uncompedFiles = uncompedFiles;
 	}	
+	
+	public UpkFileNotDecompressedException() {
+	}
+	
+	public List<Path> getFiles() {
+		return this.uncompedFiles;
+	}
 }
