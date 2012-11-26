ba757a440a3561aa0e9c1ea14c8a57b96d13c841
Daniel Kemp <twinjudgewarriro@hotmail.com>
2012-11-26 15:56:04 +1000

    Minor name change

--- src/org/xcom/mod/pojos/ResFile.java
+++ src/org/xcom/mod/pojos/ResFile.java
@@ -15,21 +15,24 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
-
 /**
  * 
  * @author Daniel Kemp
  */
 @Entity
 @Table(name = "modfile", catalog = "xmodstore", schema = "")
-@XmlRootElement(name="ResFile")
-@XmlType(propOrder={"id", "resName", "upkFilename", "bytesSum", "searchHashLength", "searchHash", "changes", "XMod"})
+@XmlRootElement(name = "ResFile")
+@XmlType(propOrder = {
+		"id", "resName", "upkFilename", "byteSum", "searchHashLength",
+		"searchHash", "changes", "XMod"
+})
 @NamedQueries({
 		@NamedQuery(name = "ResFile.findAll", query = "SELECT r FROM ResFile r"),
 		@NamedQuery(name = "ResFile.findById", query = "SELECT r FROM ResFile r WHERE r.id = :id"),
 		@NamedQuery(name = "ResFile.findByUpkFilename", query = "SELECT r FROM ResFile r WHERE r.upkFilename = :upkFilename"),
 		@NamedQuery(name = "ResFile.findBySearchHashLength", query = "SELECT r FROM ResFile r WHERE r.searchHashLength = :searchHashLength"),
-		@NamedQuery(name = "ResFile.findByBytesSum", query = "SELECT r FROM ResFile r WHERE r.bytesSum = :bytesSum") })
+		@NamedQuery(name = "ResFile.findByByteSum", query = "SELECT r FROM ResFile r WHERE r.byteSum = :byteSum")
+})
 public class ResFile implements Serializable, ModXml {
 	private static final long serialVersionUID = 1L;
 	
@@ -51,14 +54,14 @@ public class ResFile implements Serializable, ModXml {
 	private int searchHashLength;
 	
 	@Basic(optional = false)
-	@Column(name = "bytesSum")
-	private int bytesSum;
+	@Column(name = "byteSum")
+	private int byteSum;
 	
 	@Basic(optional = false)
 	@Lob
 	@Column(name = "searchHash")
 	private String searchHash;
-
+	
 	@Basic(optional = false)
 	@Column(name = "isInstalled")
 	private boolean isInstalled;
@@ -69,116 +72,115 @@ public class ResFile implements Serializable, ModXml {
 	@JoinColumn(name = "xmod", referencedColumnName = "id")
 	@ManyToOne(optional = false, fetch = FetchType.LAZY)
 	private XMod xMod;
-
+	
 	// Clean constructor required for serialisation
-	public ResFile() {
-	}
-
+	public ResFile() {}
+	
 	public ResFile(Integer id) {
 		this.id = id;
 	}
-
-	public ResFile(Integer id, String resName, String filename, String searchHash,
-			int hashLength, int sum) {
+	
+	public ResFile(Integer id, String resName, String filename,
+			String searchHash, int hashLength, int sum) {
 		this.id = id;
 		this.resName = resName;
 		this.upkFilename = filename;
 		this.searchHash = searchHash;
 		this.searchHashLength = hashLength;
-		this.bytesSum = sum;
+		this.byteSum = sum;
 	}
-
+	
 	@XmlAttribute(name = "Id")
 	public Integer getId() {
 		return id;
 	}
-
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
 	
-	@XmlElement(name="ResourceName")
+	@XmlElement(name = "ResourceName")
 	public String getResName() {
 		return resName;
 	}
-
+	
 	public void setResName(String resName) {
 		this.resName = resName;
 	}
-
-	@XmlElement(name="UpkFilename")
+	
+	@XmlElement(name = "UpkFilename")
 	public String getUpkFilename() {
 		return upkFilename;
 	}
-
+	
 	public void setUpkFilename(String filename) {
 		this.upkFilename = filename;
 	}
-
-	@XmlElement(name="SearchHash")
+	
+	@XmlElement(name = "SearchHash")
 	public String getSearchHash() {
 		return searchHash;
 	}
-
+	
 	public void setSearchHash(String searchHash) {
 		this.searchHash = searchHash;
 	}
-
-	@XmlElement(name="SearchHashLength")
+	
+	@XmlElement(name = "SearchHashLength")
 	public int getSearchHashLength() {
 		return searchHashLength;
 	}
-
+	
 	public void setSearchHashLength(int hashLength) {
 		this.searchHashLength = hashLength;
 	}
-
-	@XmlElement(name="BytesSum")
-	public int getBytesSum() {
-		return bytesSum;
-	}
-
-	public void setBytesSum(int sum) {
-		this.bytesSum = sum;
+	
+	@XmlElement(name = "ByteSum")
+	public int getByteSum() {
+		return byteSum;
 	}
 	
-	//@XmlElement(name = "IsInstalled")
+	public void setByteSum(int sum) {
+		this.byteSum = sum;
+	}
+	
+	// @XmlElement(name = "IsInstalled")
 	@XmlTransient
 	public Boolean getIsInstalled() {
 		return isInstalled;
 	}
-
+	
 	public void setIsInstalled(Boolean b) {
 		this.isInstalled = b;
 	}
-
-	@XmlElement(name="XMod")
+	
+	@XmlElement(name = "XMod")
 	public XMod getXMod() {
 		return xMod;
 	}
-
+	
 	public void setXMod(XMod xMod) {
 		this.xMod = xMod;
 	}
-
-	//@XmlTransient - if go to SOAP or REST may need to re think 
-	@XmlElementWrapper(name="Changes")
-	@XmlElement(name="HexEdit")
+	
+	// @XmlTransient - if go to SOAP or REST may need to re think
+	@XmlElementWrapper(name = "Changes")
+	@XmlElement(name = "HexEdit")
 	public List<HexEdit> getChanges() {
 		return changes;
 	}
-
+	
 	public void setChanges(List<HexEdit> edits) {
 		this.changes = edits;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		int hash = 0;
 		hash += (id != null ? id.hashCode() : 0);
 		return hash;
 	}
-
+	
 	@Override
 	public boolean equals(Object object) {
 		// TODO: Warning - this method won't work in the case the id fields are
@@ -193,26 +195,26 @@ public class ResFile implements Serializable, ModXml {
 		}
 		return true;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "org.xcom.mod.entities.UpkFile[ id=" + id + " ]";
 	}
-
+	
 	@Override
 	public Path getBasePath() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 	@Override
 	public Path getXmlSavePath() {
 		return null;
 	}
-
+	
 	@Override
 	public String getPrintName() {
 		return "RESOURCE FILE";
 	}
-
+	
 }
